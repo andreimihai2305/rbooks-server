954d778cae7f1323730d5769205febb677c5e26f
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-04 11:50:24 +0300

    adding cors for frontend api calls

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import express from 'express';
 import demoBooks from './demoBooks';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello World!");
+    res.status(200).json("Hello World!");
 });
 
 app.get('/books-list', (req, res) => {
     res.status(200).json(demoBooks);
 });
 
-const port: number = 3000;
+const port: number = 3001;
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
