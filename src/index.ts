43caf1f4745cf234c97409a9b8df5dc93ac4b85f
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-07 14:59:12 +0300

    adding some basic routes for testing purposes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import demoBooks from "./demoBooks";
-import demoUsers from "./demoUsers";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import seed from "../prisma/seed";
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { connect } from "http2";
+import { request } from "http";
 
-// const prisma = new PrismaClient();
-seed();
+const prisma = new PrismaClient();
+
 dotenv.config();
 const app = express();
 
@@ -18,13 +17,34 @@ app.get("/", (req, res) => {
   res.status(200).json("Hello World!");
 });
 
-app.get("/books-list", (req, res) => {
-  res.status(200).json(demoBooks);
-  console.log(req.headers.origin, "\n");
+app.get("/books-list", async (req, res) => {
+  // res.status(200).json("Books");
+  const books = await prisma.book.findMany();
+  if (books) return res.status(200).json(books);
+  return res.status(500).send("Internal server error");
 });
 
-app.get("/users", (req, res) => {
-  res.status(200).json(demoUsers);
+app.post("/new-book", async (req, res) => {
+  const { title, subtitle, yearPublished, authorId } = req.body;
+  const book = await prisma.book.create({
+    data: {
+      title: title,
+      yearPublished: yearPublished,
+      subtitle: subtitle ?? "",
+      Author: { connect: { id: authorId } },
+    },
+  });
+
+  if (book) return res.status(200).json(book);
+
+  return res.status(400).send("Bad request");
+});
+
+app.get("/authors", async (req, res) => {
+  const authors = await prisma.author.findMany();
+  return authors
+    ? res.status(200).json(authors)
+    : res.status(500).send("Server error");
 });
 
 app.get("/user", (req, res) => {
