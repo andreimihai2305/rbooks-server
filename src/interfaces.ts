a750d690f02fce1f1cd13d486db18291f83f838e
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-07 18:17:06 +0300

    adding ts interfaces for project

---
+++ src/interfaces.ts
@@ -0,0 +1,27 @@
+import { type } from "os";
+
+export interface User {
+  id?: string;
+  username: string;
+  password: string;
+  isAdmin?: boolean | undefined | null;
+  savedBooks?: Book[];
+  readBooks?: Book[];
+  currentlyReading?: Book[];
+}
+
+export interface Author {
+  id?: string;
+  name: string;
+  birthYear: number;
+  yearOfDeath?: number | undefined | null;
+  writtenBooks?: Book[];
+}
+
+export interface Book {
+  id?: number;
+  title: string;
+  subtitle?: string | undefined | null;
+  yearPublished: number;
+  Author?: Author;
+}
