dac41b32f3fda3a3be89ddd446e63a44f5a8c424
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-14 13:14:34 +0300

    extracting register and sign in controllers

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,47 +1,11 @@
 import { Router } from "express";
-import { User } from "../src/interfaces";
-import prisma from "../src/prismaClient";
+import registerUserController from "../controllers/registerController";
+import signInController from "../controllers/signInController";
 
-const authRouter = Router();
-authRouter.post("/sign-in", async (req, res) => {
-  const { username, password }: User = req?.body ?? {
-    username: "",
-    password: "",
-  };
-  if (!username || !password) return res.status(400).json("Bad Request");
+const authRouter: Router = Router();
 
-  const foundUser = await prisma.user.findFirst({
-    where: { username: username },
-  });
+authRouter.post("/sign-in", signInController);
 
-  if (!foundUser) return res.status(401).json({ error: "No user found" });
-  return res.status(200).json(foundUser);
-});
-
-authRouter.post("/register-user", async (req, res) => {
-  const { username, password }: User = req?.body;
-  if (!username || !password || password.length <= 5 || username.length <= 3) {
-    return res.status(400).json("Bad Request");
-  }
-  const userFromDb: User | null = await prisma.user.findFirst({
-    where: {
-      username,
-    },
-  });
-
-  if (userFromDb) {
-    return res.status(403).json("Username already used");
-  }
-
-  const newUser: User | null = await prisma.user.create({
-    data: {
-      username: username,
-      password: password,
-    },
-  });
-
-  if (newUser) return res.status(200).json(newUser);
-  return res.status(500).json("Internal Server Error");
-});
+authRouter.post("/register", registerUserController);
 
 export default authRouter;
