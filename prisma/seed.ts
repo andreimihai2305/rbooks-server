956ad9de16923e9b8a8a3dff30953c50a1f675ff
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-14 12:55:11 +0300

    updating db seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { User, Book, Author } from "../interfaces";
 const prisma = new PrismaClient();
 
 async function seed() {
@@ -6,50 +7,59 @@ async function seed() {
   await prisma.book.deleteMany();
   await prisma.author.deleteMany();
 
-  let nietzsche = await prisma.author.create({
+  let nietzsche: Author = await prisma.author.create({
     data: { name: "Friedrich Nietzsche", birthYear: 1844, yearOfDeath: 1900 },
   });
-  const jung = await prisma.author.create({
+  const cioran: Author = await prisma.author.create({
+    data: { name: "Cioran", birthYear: 1911, yearOfDeath: 1995 },
+  });
+  const jung: Author = await prisma.author.create({
     data: { name: "Carl Gustav Jung", birthYear: 1875, yearOfDeath: 1961 },
   });
 
-  const Books = await prisma.book.createMany({
+  const Books: Prisma.BatchPayload = await prisma.book.createMany({
     data: [
       {
         title: "Beyond Good and Evil",
         subtitle: "Prelude to a Philosophy of the Future",
         yearPublished: 1886,
-        authorId: nietzsche.id,
+        authorId: nietzsche.id!,
       },
       {
         title: "Ecce Homo",
         subtitle: "How One Becomes What One Is",
         yearPublished: 1906,
-        authorId: nietzsche.id,
+        authorId: nietzsche.id!,
       },
       {
         title: "Man and his symbols",
         yearPublished: 1906,
-        authorId: jung.id,
+        authorId: jung.id!,
+      },
+      {
+        title: "On the heights of despair",
+        yearPublished: 1934,
+        authorId: cioran.id!,
       },
     ],
   });
 
-  const beyondGoodAndEvil = await prisma.book.findFirst({
+  const beyondGoodAndEvil: Book | null = await prisma.book.findFirst({
     where: {
       title: "Beyond Good and Evil",
     },
   });
 
-  const andrew = await prisma.user.create({
+  const andrew: User = await prisma.user.create({
     data: {
       username: "andrew",
       password: "helloworld",
       isAdmin: true,
       savedBooks: { connect: { id: beyondGoodAndEvil?.id } },
+      currentlyReading: { connect: { id: beyondGoodAndEvil?.id } },
     },
   });
-  const john = await prisma.user.create({
+  const john: User = await prisma.user.create({
     data: {
       username: "john",
       password: "helloandrew",
