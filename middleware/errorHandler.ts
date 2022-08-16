81755b7cbff1d80b1cf804fac6c5f1b190445497
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-16 11:02:44 +0300

    added error logging

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -1,13 +1,16 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
+import { log } from "./logger";
 
-const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const errorHandler = (err: Error, req: Request, res: Response) => {
+  log(
+    `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+    "errorLogs.log"
+  );
+
   console.log(err.stack);
-  return res.status(500).json(err.message);
+  const status = res.statusCode ? res.statusCode : 500;
+
+  return res.status(status).json({ message: err.message });
 };
 
 export default errorHandler;
